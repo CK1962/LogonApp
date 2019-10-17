d54200041ebf319cd23cabd7615a0ad29dcd3fe3
ck1962 <ckeslin@gmail.com>
2019-10-16 19:19:10 -0500

    initial commit

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { AuthService } from "../services/auth.service";
+import { first } from "rxjs/operators";
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-login',
@@ -6,10 +10,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  loginForm : FormGroup;
 
-  constructor() { }
+  constructor(
+    private formbuilder: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+    ){ }
 
   ngOnInit() {
+    this.loginForm = this.formbuilder.group({
+      emaill:" ",
+      password: " "
+    });
   }
 
+onSubmit (loginData) {
+  //stop here if form is invalid
+  if (this.loginForm.invalid) {
+    return;
+  }
+  this.authService
+  .login(loginData.email, loginData.password)
+  .pipe(first())
+            .subscribe(
+                data => {
+                    this.router.navigateByUrl("/");
+                },
+                error => {
+                    console.log(error);
+                });
+}
 }
